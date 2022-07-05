25cd1f80fa7efd6e0423dbc85de5eca71cdcd3f0
Carlos Rangel <charlesleimaginant@gmail.com>
2022-07-04 21:27:14 -0500

    Use object destructuring in renderCharacter() function

--- index.js
+++ index.js
@@ -81,14 +81,19 @@ const monster = {
     diceRoll: 4,
 };
 
+// CHALLENGE
+// 1. Deconstruct the data object 
+// 2. Update the template string as necessary
+
 function renderCharacter(data) {
-    document.getElementById(data.elementId).innerHTML =
+    const {elementId, name, avatar, health, diceRoll} = data;
+    document.getElementById(elementId).innerHTML =
         `<div class="character-card">
-            <h4 class="name"> ${data.name} </h4>
-            <img class="avatar" src="${data.avatar}" />
-            <div class="health">health: <b> ${data.health} </b></div>
+            <h4 class="name"> ${name} </h4>
+            <img class="avatar" src="${avatar}" />
+            <div class="health">health: <b> ${health} </b></div>
             <div class="dice-container">
-                <div class="dice"> ${data.diceRoll} </div>
+                <div class="dice"> ${diceRoll} </div>
             </div>
         </div>`
 }
