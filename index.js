d4a224a847fa272299316c96e58e4646d3c03f51
Carlos Rangel <charlesleimaginant@gmail.com>
2022-07-07 01:01:39 -0500

    Generate random dices by returing a function into a function

--- index.js
+++ index.js
@@ -63,6 +63,35 @@ const monsterHealth = "10";
 const monsterDiceRoll = 4;
 */
 
+/*
+Challenge 
+1. Create a function called getDiceHtml. 
+2. getDiceHtml should map over the array of dice rolls 
+   returned from getDiceRollArray to generate the html 
+   we need to render our dice with random values. This is 
+   the HTML: `<div class="dice">DICE VALUE HERE</div>`
+3. Think about the parameters and arguments!
+4. Down in renderCharacter(), set diceHtml equals to our 
+   new getDiceHtml function. Remember to give it the argument
+   it needs. 
+5. Delete any code we no longer need.
+**hint.md for help**
+*/
+
+function getDiceRollArray(diceCount) {
+    let newDiceRolls = [];
+    for (let i = 0; i < diceCount; i++) {
+        newDiceRolls.push(Math.floor(Math.random() * 6) + 1);
+    }
+    return newDiceRolls;
+}
+
+function getDiceHtml(diceCount){
+    return getDiceRollArray(diceCount).map(function(num){
+        return `<div class="dice">${num}</div>`
+    }).join('')
+}
+
 
 
 
@@ -70,8 +99,7 @@ const hero = {
     elementId: "hero", 
     name: "Wizard", 
     avatar: "images/wizard.png", 
-    health: 60, 
-    diceRoll: [3, 1, 4],
+    health: 60,
     diceCount: 3,
 };
 
@@ -79,8 +107,7 @@ const monster = {
     elementId: "monster", 
     name: "Orc", 
     avatar: "images/orc.png", 
-    health: 10, 
-    diceRoll: [2],
+    health: 10,
     diceCount: 1,
 };
 
@@ -104,10 +131,8 @@ and save the new array to diceHTML.
 */
 
 function renderCharacter(data) {
-    const { elementId, name, avatar, health, diceRoll, diceCount } = data;
-    const diceHtml = diceRoll.map(function(num){
-        return `<div class="dice">${num}</div>`;
-    }).join('');
+    const { elementId, name, avatar, health, diceCount } = data;
+    const diceHtml = getDiceHtml(diceCount);
 
     document.getElementById(elementId).innerHTML =
         `<div class="character-card">
