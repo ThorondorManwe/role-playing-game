42b1fa3bb80b2d10be9a39294ffd7f1540e71f5f
Carlos Rangel <charlesleimaginant@gmail.com>
2022-07-08 13:42:57 -0500

    Render placeholder

--- Character.js
+++ Character.js
@@ -1,18 +1,21 @@
 import {getDiceRollArray} from './utils.js';
+import { getDicePlaceholderHtml } from './utils.js';
 
 
 function Character(data) {
     Object.assign(this, data)
     
-     this.getDiceHtml = function(diceCount) {
-        return getDiceRollArray(diceCount).map(function(num){ 
-            return  `<div class="dice">${num}</div>`
+    this.diceArray = getDicePlaceholderHtml(this.diceCount)
+    
+    this.getDiceHtml = function() {
+        this.currentDiceScore = getDiceRollArray(this.diceCount)
+        this.diceArray = this.currentDiceScore.map(function(num){
+            return `<div class="dice">${num}</div>`
         }).join('')
-    }
-
+    } 
+    
     this.getCharacterHtml = function () {
         const { elementId, name, avatar, health, diceCount } = this;      
-        let diceHtml = this.getDiceHtml(diceCount);
         
            return `
             <div class="character-card">
@@ -20,9 +23,9 @@ function Character(data) {
                 <img class="avatar" src="${avatar}" />
                 <div class="health">health: <b> ${health} </b></div>
                 <div class="dice-container">
-                    ${diceHtml}
+                    ${this.diceArray}
                 </div>
-            </div>`;
+            </div>`
     }  
 }
 
