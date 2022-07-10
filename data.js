64ea48235d305c4fa4502c2013ca07fc414153e6
Carlos Rangel <charlesleimaginant@gmail.com>
2022-07-10 00:38:15 -0500

    Add a life bar to characters

--- data.js
+++ data.js
@@ -9,10 +9,10 @@ const characterData = {
     monster: {
         name: "Orc",
         avatar: "images/orc.png",
-        health: 10,
+        health: 40,
         diceCount: 1,
         currentDiceScore: []
     }
-};
+}
 
 export default characterData;
